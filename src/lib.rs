3c1c610d78de11c7b205e944072367707e8b3aed
André Vennberg <andre.vennberg@uncloudy.se>
2021-10-14 15:44:24 +0200

    feat(all): initial commit

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+#![allow(unknown_lints)]
+#![deny(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
+)]
+#![warn(
+    clippy::pedantic,
+    clippy::cargo,
+    clippy::perf,
+    clippy::nursery,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::clone_on_ref_ptr,
+    clippy::create_dir,
+    clippy::decimal_literal_representation,
+    clippy::default_numeric_fallback,
+    clippy::exit,
+    clippy::expect_used,
+    clippy::filetype_is_file,
+    clippy::if_then_some_else_none,
+    clippy::indexing_slicing,
+    clippy::inline_asm_x86_att_syntax,
+    clippy::let_underscore_must_use,
+    clippy::lossy_float_literal,
+    clippy::map_err_ignore,
+    clippy::mem_forget,
+    clippy::multiple_inherent_impl,
+    clippy::panic_in_result_fn,
+    clippy::rc_buffer,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::semicolon_if_nothing_returned,
+    clippy::str_to_string,
+    clippy::string_to_string,
+    clippy::todo,
+    clippy::unimplemented,
+    clippy::unneeded_field_pattern,
+    clippy::unreachable,
+    clippy::unwrap_in_result,
+    clippy::unwrap_used,
+    clippy::verbose_file_reads,
+    clippy::wildcard_enum_match_arm,
+    clippy::wrong_self_convention
+)]
+#![allow(
+    clippy::non_ascii_literal,
+    // clippy::cargo_common_metadata,
+    // clippy::multiple_crate_versions
+)]
+
+//! Rust wrapper for the Holodex v2 API.
