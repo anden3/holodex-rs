0689e30cc62bd2bd93a110f6a95e74ba9713bbbc
André Vennberg <andre.vennberg@gmail.com>
2021-11-14 15:15:45 +0100

    refactor(errors): replace `thiserror` and `miette` with `quick_error`

--- src/errors.rs
+++ src/errors.rs
@@ -1,86 +1,108 @@
 //! Types for various errors that can occur when interacting with the API.
 
-use miette::Diagnostic;
-use thiserror::Error;
+use quick_error::quick_error;
 
-#[derive(Error, Diagnostic, Debug)]
-/// Errors that can occur when interacting with the Holodex API.
-pub enum Error {
-    #[error("API token contains invalid characters.")]
-    /// The API token provided to the client is invalid.
-    InvalidApiToken,
-    #[error("Error creating HTTP client: {0:?}")]
-    /// An error occurred while creating the HTTP client.
-    HttpClientCreationError(#[source] reqwest::Error),
-    #[error("Error sending request to {endpoint}: {source:?}")]
-    /// An error occurred while sending an API request.
-    ApiRequestFailed {
-        /// The endpoint that was queried.
-        endpoint: &'static str,
-        #[source]
-        /// The error that was encountered.
-        source: reqwest::Error,
-    },
-    #[error("Invalid response received from endpoint ({endpoint}).")]
-    /// The API returned a faulty response or server error.
-    InvalidResponse {
-        /// The endpoint that was queried.
-        endpoint: &'static str,
-        #[source]
-        /// The error that was encountered.
-        source: ValidationError,
-    },
-    #[error("The provided video ID was not valid: {0}")]
-    /// An invalid video ID was passed to the API.
-    InvalidVideoId(String),
-
-    #[error("The provided channel ID was not valid: {0}")]
-    /// An invalid channel ID was passed to the API.
-    InvalidChannelId(String),
-
-    #[error("The filter could not be constructed due to invalid arguments.")]
-    /// A filter could not be constructed due to invalid arguments.
-    FilterCreationError(String),
+quick_error! {
+    #[derive(Debug)]
+    #[allow(missing_docs)]
+    /// Errors that can occur when interacting with the Holodex API.
+    pub enum Error {
+        /// The API token provided to the client is invalid.
+        InvalidApiToken {
+            display("API token contains invalid characters.")
+        }
+        /// An error occurred while creating the HTTP client.
+        HttpClientCreationError(err: ureq::Error) {
+            display("Error creating HTTP client: {:?}", err)
+            source(err)
+        }
+        /// An error occurred while sending a request to the API.
+        ApiRequestFailed { source: ureq::Error, endpoint: &'static str } {
+            display("Error sending request to {}: {:?}", endpoint, source)
+            source(source)
+        }
+        /// The API returned a faulty response or server error.
+        InvalidResponse { source: ValidationError, endpoint: &'static str } {
+            display("Invalid response received from {}: {:?}", endpoint, source)
+            source(source)
+        }
+        /// An invalid video ID was passed to the API.
+        InvalidVideoId(id: String) {
+            display("The provided video ID was not valid: {}", id)
+        }
+        /// An invalid channel ID was passed to the API.
+        InvalidChannelId(id: String) {
+            display("The provided channel ID was not valid: {}", id)
+        }
+        /// A filter could not be constructed due to invalid arguments.
+        FilterCreationError(err: String) {
+            display("The filter could not be constructed due to invalid arguments: {}", err)
+        }
+    }
 }
 
-#[derive(Error, Diagnostic, Debug)]
-/// Errors that can occur when validating a response from the Holodex API.
-pub enum ValidationError {
-    #[error("Server error: {0:?}")]
-    /// The API returned a server error.
-    ServerError(#[from] ServerError),
-    #[error("Parse error: {0:?}")]
-    /// The response from the API could not be parsed.
-    ParseError(#[from] ParseError),
+quick_error! {
+    #[derive(Debug)]
+    /// Errors that can occur when validating a response from the Holodex API.
+    pub enum ValidationError {
+        /// The API returned a server error.
+        ServerError(err: ServerError) {
+            display("Server error: {}", err)
+            from()
+        }
+        /// The response from the API could not be parsed.
+        ParseError(err: ParseError) {
+            display("Parse error: {}", err)
+            from()
+        }
+    }
 }
 
-#[derive(Error, Diagnostic, Debug)]
-/// Errors that occur when the API returns an error code.
-pub enum ServerError {
-    #[error("Server returned an error code: {0}")]
-    /// The API returned an error code.
-    ErrorCode(#[from] reqwest::Error),
-    #[error("Server returned error {0} with message: {1}")]
-    /// The API returned an error code with a message.
-    ErrorCodeWithValue(#[source] reqwest::Error, String),
-    #[error("Server returned error {0} with a message that could not be parsed: {1:?}")]
-    /// The API returned an error code with a message that could not be parsed.
-    ErrorCodeWithValueParseError(#[source] reqwest::Error, ParseError),
+quick_error! {
+    #[derive(Debug)]
+    /// Errors that occur when the API returns an error code.
+    pub enum ServerError {
+        /// The API returned an error code.
+        ErrorCode(code: u16) {
+            display("Server returned an error code: {}", code)
+            from()
+        }
+        /// The API returned an error code with a message.
+        ErrorCodeWithValue(code: u16, message: String) {
+            display("Server returned an error message: [{}] {}", code, message)
+        }
+        /// The API returned an error with a message that could not be parsed.
+        ErrorCodeWithValueParseError(code: u16, source: ParseError) {
+            display("Server returned code {} with a message that could not be parsed: {:?}", code, source)
+            from(source)
+        }
+    }
 }
 
-#[derive(Error, Diagnostic, Debug)]
-/// Errors that occur when parsing a response from the API.
-pub enum ParseError {
-    #[error("Could not decode response: {0:?}")]
-    /// The response from the API could not be converted into bytes.
-    ResponseDecodeError(#[source] reqwest::Error),
-    #[error("Failed to parse response as JSON: {0:?}\nResponse: {1}")]
-    /// The response from the API could not be parsed as JSON.
-    ResponseJsonParseError(#[source] serde_json::Error, String),
-    #[error("Failed to parse response: {0:?}\nResponse: {1}")]
-    /// The response from the API could not be parsed.
-    ResponseParseError(#[source] serde_json::Error, serde_json::Value),
-    #[error("Response was neither valid JSON nor valid UTF-8.")]
-    /// The response from the API could not be parsed as JSON or UTF-8.
-    ResponseUtf8Error(#[from] std::str::Utf8Error),
+quick_error! {
+    #[derive(Debug)]
+    /// Errors that occur when parsing a response from the API.
+    pub enum ParseError {
+        /// The response from the API could not be converted into bytes.
+        ResponseDecodeError(err: std::io::Error) {
+            display("Could not decode response: {}", err)
+            source(err)
+        }
+        /// The response from the API could not be parsed as JSON.
+        ResponseJsonParseError(err: serde_json::Error, response: String) {
+            display("Failed to parse response as JSON: {}\nResponse: {}", err, response)
+            source(err)
+        }
+        /// The response from the API could not be parsed.
+        ResponseParseError(err: serde_json::Error, response: serde_json::Value) {
+            display("Failed to parse response: {}\nResponse: {}", err, response)
+            source(err)
+        }
+        /// The response from the API could not be parsed as JSON or UTF-8.
+        ResponseUtf8Error(err: std::str::Utf8Error) {
+            display("Response was neither valid JSON nor valid UTF-8.")
+            source(err)
+            from()
+        }
+    }
 }
